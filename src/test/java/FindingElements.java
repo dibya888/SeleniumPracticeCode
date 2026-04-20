88f121217cc99d649a10181b48fda30f4af00bee
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-04-20 12:15:47 +0600

    Finding New Elements: By ID

---
+++ src/test/java/FindingElements.java
@@ -0,0 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class FindingElements {
+    static void main(String[] args) throws InterruptedException {
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+//        driver.findElement(By.id());
+
+
+
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
